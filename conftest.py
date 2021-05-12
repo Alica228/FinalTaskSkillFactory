49b5aecdb5a30e31402ff7cc70941355bf362a34
luna <Yura25051999@yandex.ru>
2021-05-12 23:08:13 +0000

    add `--headless` to chrome's options

--- conftest.py
+++ conftest.py
@@ -23,7 +23,7 @@ def pytest_runtest_makereport(item, call):
 @pytest.fixture
 def chrome_options(chrome_options):
     # chrome_options.binary_location = '/usr/bin/google-chrome-stable'
-    # chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--log-level=DEBUG')
 
